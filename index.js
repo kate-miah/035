4eba176bec1a69bf02b2ecc154708337b3498ccd
kate-miah <ekaterin89@gmail.com>
2025-07-23 22:45:31 -0500

    const + Object

--- index.js
+++ index.js
@@ -20,3 +20,56 @@ for (let i = 2; i <= 3; i++) {
     }
         
     }
+/*
+function sayHello(name, lastName, age) {
+    let hello = '';
+    if (age >5 && age < 18) {
+        hello = 'Hi'
+    } else if (age > 18 && age < 30) {
+        hello = 'Hello'
+    } else {
+        hello = 'Good Day'
+    }
+    console.log(`${hello}, ${name} ${lastName}`);
+}
+
+    let user = {
+        name: 'John',
+        lastName: 'Doe',
+        age: 20
+    }
+sayHello(user.name, user.lastName, user.age);
+*/
+
+// або
+
+function sayHello(user) {
+    let hello = '';
+    if (user.age >5 && user.age < 18) {
+        hello = 'Hi'
+    } else if (user.age > 18 && user.age < 30) {
+        hello = 'Hello'
+    } else {
+        hello = 'Good Day'
+    }
+    console.log(`${hello}, ${user.name} ${user.lastName}`);
+}
+
+    let user = {
+        name: 'John',
+        lastName: 'Doe',
+        age: 20
+    }
+sayHello(user);
+
+
+// const
+
+let a = 5;
+a = 10;
+
+const b = 10;
+// b = 15; //TypeError: Assignment to constant variable.
+const obj = {
+    a: 'value'
+}
