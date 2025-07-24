d7262153fede99a3db0bd0bfefc005e3dfa279c6
kate-miah <ekaterin89@gmail.com>
2025-07-23 22:02:40 -0500

    035-task

---
+++ index.js
@@ -0,0 +1,11 @@
+let cup = {
+    color: 'green',
+    volume: '250 ml',
+    form: 'bowl'
+}
+let prop1 = 'color';
+let prop2 = 'volume';
+let prop3 = 'form';
+console.log(cup[prop1]);
+console.log(cup[prop2]);
+console.log(cup[prop3]);
