7dce64ecb73049c88952cf1b29bf8348adb21450
kate-miah <ekaterin89@gmail.com>
2025-07-23 22:17:35 -0500

    template string

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ let prop3 = 'form';
 console.log(cup[prop1]);
 console.log(cup[prop2]);
 console.log(cup[prop3]);
+
+
+
+// Таблиця множення 2 і 3
+
+for (let i = 2; i <= 3; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j}`)
+    }
+        
+    }
